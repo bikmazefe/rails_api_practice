8318fa5923f251b035e85f00cadea48e98230692
Efe Bikmaz <bikmazefe@gmail.com>
2021-01-24 18:52:04 +0300

    first commit

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,47 @@
+module Api
+    module V1 
+        class BooksController < ApplicationController
+            MAX_PAGINATION_LIMIT =  100
+
+            def index
+                books = Book.limit(limit).offset(params[:offset])
+                render json: BooksRepresenter.new(books).as_json
+            end
+    
+            def create
+                author = Author.create!(author_params)
+                book = Book.new(book_params.merge(author_id: author.id))
+                UpdateSkuJob.perform_later(book_params[:title])
+
+                if book.save
+                    render json: BookRepresenter.new(book).as_json, status: :created
+                else
+                    render json: book.errors, status: :unprocessable_entity
+                end
+            end
+    
+            def destroy
+                book = Book.find(params[:id]).destroy!
+                head :no_content 
+            end
+    
+            private
+
+            def limit 
+                [
+                    params.fetch(:limit, 100).to_i,
+                    MAX_PAGINATION_LIMIT
+                ].min
+            end
+
+            def author_params
+                params.require(:author).permit(:first_name, :last_name, :age)
+            end
+
+            def book_params
+                params.require(:book).permit(:title)
+            end
+        end
+    end
+end
+
