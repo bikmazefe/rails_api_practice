84d8084809023a976924043ad5e241311c8c3648
Efe Bikmaz <bikmazefe@gmail.com>
2021-01-25 22:12:16 +0300

    JWT Authentication setup

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 describe 'Authentication', type: :request do 
     describe 'POST Authenticate' do 
+        let(:user) { FactoryBot.create(:user, username: "BookSeller99", password: 'Password1') }
         it 'authenticates the client' do 
             post '/api/v1/authenticate', params: {
-                username: "BookSeller99",
-                password: "Password1"
+                username: user.username,
+                password: user.password
             }
 
             expect(response).to have_http_status(:created)
             expect(response_body).to eq({
-                    'token' => '123'
+                    'token' => "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w"
                 })
         end
 
@@ -34,6 +35,14 @@ describe 'Authentication', type: :request do
             expect(response_body).to eq ({
                 'error' => 'param is missing or the value is empty: password'
             })
+        end
+
+        it 'returns error when password is incorrect' do 
+            post '/api/v1/authenticate', params: {
+                username: user.username,
+                password: "incorrect"
+            }
+            expect(response).to have_http_status(:unauthorized)
         end
     end
 end
